f1214aa5d1b8f86f5f699ec24d731f60403929d3
ryuga <ryuga.private@gmail.com>
2021-09-24 12:30:04 +0900

    initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  title: string = 'ReactiveForm';
+  reactiveText = new FormControl('');
+
+  constructor() {}
+}
